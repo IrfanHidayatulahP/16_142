9202178f3dcd9fa469b7455e96606f45cb43c955
IrfanHidayatulahP <irfanhputra400@gmail.com>
2025-01-20 21:19:26 +0700

    membuat interface AsetRepository dan class NetworkAsetRepository

---
+++ app/src/main/java/com/example/finalprojectpam/Repository/NetworkAsetRepository.kt
@@ -0,0 +1,42 @@
+package com.example.finalprojectpam.Repository
+
+import com.example.finalprojectpam.model.Aset
+import com.example.finalprojectpam.service_api.AsetApiService
+
+interface AsetRepository {
+    suspend fun getAset() : List<Aset>
+    suspend fun insertAset(aset: Aset)
+    suspend fun updateAset(id_aset: String, aset: Aset)
+    suspend fun deleteAset(id_aset: String)
+}
+
+class NetworkAsetRepository (
+    private val asetApiService: AsetApiService
+) : AsetRepository {
+    override suspend fun getAset(): List<Aset> =
+        asetApiService.getAset()
+
+    override suspend fun insertAset(aset: Aset) {
+        asetApiService.insertAset(aset)
+    }
+
+    override suspend fun updateAset(id_aset: String, aset: Aset) {
+        asetApiService.updateAset(id_aset, aset)
+    }
+
+    override suspend fun deleteAset(id_aset: String) {
+        try {
+            val response = asetApiService.deleteAset(id_aset)
+            if (!response.isSuccessful) {
+                throw Exception("Failed to delete Aset. HTTP Status Code: ${response.code()}")
+            }
+            else {
+                response.message()
+                println(response.message())
+            }
+        }
+        catch (e: Exception) {
+            throw e
+        }
+    }
+}
