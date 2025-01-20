0e3522d399350d5dac52e1b9c6bc70817ad6412b
IrfanHidayatulahP <irfanhputra400@gmail.com>
2025-01-20 15:42:20 +0700

    membuat data class dan service api untuk tabel Kategori

---
+++ app/src/main/java/com/example/finalprojectpam/model/Kategori.kt
@@ -0,0 +1,9 @@
+package com.example.finalprojectpam.model
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class Kategori(
+    val id_kategori : String,
+    val nama_kategori : String
+)
---
+++ app/src/main/java/com/example/finalprojectpam/service_api/KategoriApiService.kt
@@ -0,0 +1,29 @@
+package com.example.finalprojectpam.service_api
+
+import com.example.finalprojectpam.model.Kategori
+import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.DELETE
+import retrofit2.http.GET
+import retrofit2.http.Headers
+import retrofit2.http.POST
+import retrofit2.http.PUT
+import retrofit2.http.Query
+
+interface KategoriApiService {
+    @Headers(
+        "Accept: application/json",
+        "Content-Type: application/json"
+    )
+    @GET("bacakategori.php")
+    suspend fun getKategori(): List<Kategori>
+
+    @POST("insertkategori.php")
+    suspend fun insertKategori(@Body kategori: Kategori)
+
+    @PUT("editkategori.php")
+    suspend fun updateKategori(@Query("id_kategori")id_kategori: String, @Body kategori: Kategori)
+
+    @DELETE("deletekategori.php")
+    suspend fun deleteKategori(@Query("id_kategori")id_kategori: String): Response<Void>
+}
